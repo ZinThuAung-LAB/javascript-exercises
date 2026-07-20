935247af1543bd6424662bc7b34fed8d11a7337b
ZinThuAung <zinthyaung@gmail.com>
2026-07-20 13:14:20 +0630

    TOP array assignments camalize and filter

---
+++ foundations/Assignments/Filter_Range/filter.js
@@ -0,0 +1,14 @@
+function filterRange(arr, a, b) {
+  return arr.filter((item) => item >= a && item <= b);
+}
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+// alert(filtered); // 3,1 (matching values)
+
+// alert(arr); // 5,3,8,1 (not modified)
+
+console.log(filtered);
+console.log(arr);
