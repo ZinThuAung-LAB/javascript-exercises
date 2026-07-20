44934351bbb88e9fe0684cd97014bc3ccf063c41
ZinThuAung <zinthyaung@gmail.com>
2026-07-20 15:20:52 +0630

    add sumAll assignment

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+function sumAll(num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  if (num1 > 0 && num2 > 0) {
+    let sum = 0;
+    if (num1 > num2) {
+      min = num2;
+      max = num1;
+    } else {
+      min = num1;
+      max = num2;
+    }
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
+}
 
-};
+console.log(sumAll(2, 4));
 
-// Do not edit below this line
 module.exports = sumAll;
