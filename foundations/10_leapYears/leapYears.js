e9ab2be31cfdb256b7fe8dc33fc3e71b298d737c
ZinThuAung <zinthyaung@gmail.com>
2026-07-20 15:33:12 +0630

    add leapYear assignment

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isDivisibleByFourHundred = year % 400 === 0;
 
+  if (isDivisibleByFour && (!isCentury || isDivisibleByFourHundred)) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(leapYears(1800));
 
 // Do not edit below this line
 module.exports = leapYears;
