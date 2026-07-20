a1bc4a99bf8e208f1a2369fefcbeec03ffb09a5a
ZinThuAung <zinthyaung@gmail.com>
2026-07-20 14:46:52 +0630

    add reverseString assignment

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+function reverseString(str) {
+  let newString = "";
 
-};
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
+
+console.log(reverseString(""));
 
 // Do not edit below this line
 module.exports = reverseString;
