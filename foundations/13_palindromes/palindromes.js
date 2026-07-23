f83fac7c0873df7d8517457c5ab8db4af09eb455
ZinThuAung <zinthyaung@gmail.com>
2026-07-23 16:58:53 +0630

    add palimdrome function using 2 pointer approach

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanStr = str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split(" ")
+    .join("");
+  console.log(cleanStr);
+  let left = 0;
+  let right = cleanStr.length - 1;
+  while (left < right) {
+    if (cleanStr[left] !== cleanStr[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 };
 
+console.log(palindromes("A car, a man, a maraca."));
 // Do not edit below this line
 module.exports = palindromes;
