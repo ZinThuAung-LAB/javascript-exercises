9c86e997fb13e4df0767dc0ba82ce7d88444dbbe
ZinThuAung <zinthyaung@gmail.com>
2026-07-23 16:16:08 +0630

    add substract funtion and test

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,27 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((accumulator, current) => {
+    return accumulator + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((a, b) => {
+    return a * b;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n < 0) return Math.NAN;
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 };
-
+console.log(multiply([2]));
 // Do not edit below this line
 module.exports = {
   add,
@@ -29,5 +35,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
