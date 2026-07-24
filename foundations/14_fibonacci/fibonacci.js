17affc90b58b4623d395390387afda9e0e1d778a
ZinThuAung <zinthyaung@gmail.com>
2026-07-24 09:58:41 +0630

    add fibonacci function recursive and work on strings also with Number(n) by changing type

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,8 @@
-const fibonacci = function() {
-
-};
-
+function fibonacci(n) {
+  n = Number(n);
+  if (isNaN(n) || n < 0) return "OOPS";
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
 // Do not edit below this line
 module.exports = fibonacci;
