8497c11f9772c30b58a9c331baabca0ba7279c52
ZinThuAung <zinthyaung@gmail.com>
2026-08-14 10:34:26 +0630

    safeguard objector constructior Book

--- My javaScript Exercise/script.js
+++ My javaScript Exercise/script.js
@@ -1,9 +1,31 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-  this.id = crypto.randomUUID(); // Assign unique identifier
+  if (!new.target) {
+    throw Error(
+      "You must use the 'new' operator to call the constructor and This is test",
+    );
+  }
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.info = function () {
+    console.log(
+      this.title + " by " + this.author + ", " + this.pages + ", " + this.read,
+    );
+  };
 }
+
+const theError = Book("Test", "Zack", "1page", "not read");
+
+const theHobbit = new Book(
+  "The Hobbit",
+  "J.R.R Tolkein",
+  "295 pages",
+  "not read yet",
+);
+
+theError.info();
+
+theHobbit.info();
